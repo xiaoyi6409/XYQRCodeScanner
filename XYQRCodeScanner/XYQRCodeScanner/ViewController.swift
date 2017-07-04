708a4155dd2a9473d541e83ea240c57abef61601
mouruiXY <mouruiXY@mouruideMac-mini-2.local>
2017-07-04 14:51:07 +0800

    project upload

---
+++ XYQRCodeScanner/XYQRCodeScanner/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  XYQRCodeScanner
+//
+//  Created by xiaoyi on 2017/6/28.
+//  Copyright © 2017年 xiaoyi. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        
+        let QRCodePriduce = XYQRCodeGeneration()
+        var QRCodeImage =    QRCodePriduce.generateQRCode(codeStr:  "http://www.baidu.com")
+       QRCodeImage = QRCodeImage.setImagePixelColor(colorStr: "068b22", alpha: 1.0)
+        let QRCodeImageView = UIImageView(frame: CGRect(x: SCREENWIDTH/2 - 200/2, y: SCREENHEIGHT/2 - 200/2, width: 200, height: 200))
+        QRCodeImageView.image = QRCodeImage
+        self.view.addSubview(QRCodeImageView)
+        
+        
+        let btn = UIButton(frame: CGRect(x:SCREENWIDTH/2 - 200/2 , y: QRCodeImageView.frame.maxY + 40, width: 200, height: 40))
+        btn.addTarget(self, action: #selector(btnAct), for: .touchUpInside)
+        btn.setTitle("扫码", for: .normal)
+        btn.setTitleColor(UIColor.yellow, for: .normal)
+        btn.backgroundColor = UIColor.green
+        self.view.addSubview(btn)
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    func btnAct(){
+        self.present(XYQRCodeScannerViewController(), animated: true, completion: nil)
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
